70650fbf4cf374652776b6c0fc4d3273dc86d003
almeida1 <engsoftware1@gmail.com>
2025-10-18 13:54:30 -0300

    refatoracao

--- src/componentes/ClienteCadastrar/ClienteCadastrarContainer.jsx
+++ src/componentes/ClienteCadastrar/ClienteCadastrarContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ClienteCadastrarView from "./ClienteCadastrarView";
-import ClienteService from "./ClienteService";
+import ClienteService from "./ClienteService"; // container consome esse servico
 //*********************************************************************************** */
 //Gerenciamento de estado e coordenação entre a view e o serviço de cadastro de clientes
 //O container gerencia o estado dos campos do formulário, lida com a submissão dos dados e navegação
